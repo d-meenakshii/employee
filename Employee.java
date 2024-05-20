96a1d45600fea625b88c81bc79def46575d9c364
d-meenakshii <dmeenakshi3995@gmail.com>
2024-05-20 14:50:33 +0530

    Created Employee.java successfully

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee
+{
+  int id;
+  String name;
+  long phone;
+  String designation;
+  int Salary;
+  
+  public static void main(String args[])
+  {
+   Employee e1 = new Employee(101,"sonu",7647930098,"Developer",50000);
+   Employee.e1(id);
+   Employee.e1(name);
+   Employee.e1(phone);
+   Employee.e1(designation);
+   Employee.e1(salary);
+  }
+}  
